cab45d32a216f8cd5063d402d21be108499f1da9
Richard Andreu <randreu@gmail.com>
2013-08-14 22:35:18 -0400

    Just added an echo message to check if tokens were succesfully set

--- SetTokens.sh
+++ SetTokens.sh
@@ -3,3 +3,7 @@
 ./authenticate.sh $1 $2 > token
 TOKEN=`cat token | grep -Po '(?<={"token":{"id":")[^"]+'`
 URL=$(cat token | grep -Po '(?<="publicURL":")[^"]+dfw.loadbalancers[^"]+' | sed 's/\\//g'  | head -n 1)
+
+echo "TOKEN set to $TOKEN"
+echo "URL set to $URL"
+
